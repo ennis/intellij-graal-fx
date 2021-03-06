127108dbf1a82b474fa8ab8449c54ab76a1aee23
GitHub Action <action@github.com>
2021-03-06 15:10:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/ennis/intellijgraalfx/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ennis/intellijgraalfx/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ennis.intellijgraalfx.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ennis.intellijgraalfx.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/ennis/intellijgraalfx/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ennis/intellijgraalfx/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ennis.intellijgraalfx.services
 
+import com.github.ennis.intellijgraalfx.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
